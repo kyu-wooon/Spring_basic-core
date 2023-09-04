658211e57a77399c351dcc906710855c470c0b41
DryLeonhard <ygangmin@gmail.com>
2023-09-04 10:50:52 +0900

    AppConfig 리펙토링

--- src/main/java/basic/core/AppConfig.java
+++ src/main/java/basic/core/AppConfig.java
@@ -1,20 +1,31 @@
 package basic.core;
 
 import basic.core.discount.FixDiscountPolicy;
+import basic.core.discount.DiscountPolicy;
+import basic.core.discount.RateDiscountPolicy;
 import basic.core.member.MemberService;
 import basic.core.member.MemberServiceImpl;
 import basic.core.member.MemoryMemberRepository;
+import basic.core.member.MemberRepository;
 import basic.core.order.OrderService;
 import basic.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
-    public MemberService memberService(){
-        return  new MemberServiceImpl(new MemoryMemberRepository());
-    }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        public MemberService memberService() {
+            return new MemberServiceImpl(memberRepository());
+        }
+        public OrderService orderService() {
+            return new OrderServiceImpl(
+                    memberRepository(),
+                    discountPolicy());
+        }
+        public MemberRepository memberRepository() {
+            return new MemoryMemberRepository();
+        }
+        public DiscountPolicy discountPolicy() {
+           // return new FixDiscountPolicy();
+            return new RateDiscountPolicy();
+        }
     }
-
-}

5148d4e89281f7f6c48d99527507a3e29fdde868
DryLeonhard <ygangmin@gmail.com>
2023-09-05 11:46:25 +0900

    spring bean 등록 확인

--- src/main/java/basic/core/OrderApp.java
+++ src/main/java/basic/core/OrderApp.java
@@ -7,13 +7,21 @@ import basic.core.order.OrderService;
 import basic.core.order.OrderServiceImpl;
 import basic.core.member.Grade;
 import basic.core.order.Order;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+        //OrderService orderService = appConfig.orderService();
+
+        //스프링 컨테이너 인터페이스
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
--- src/main/java/basic/core/member/MemberApp.java
+++ src/main/java/basic/core/member/MemberApp.java
@@ -1,14 +1,21 @@
 package basic.core.member;
 
 import basic.core.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-
-        MemberService memberService = appConfig.memberService();//멤버서비스 생성
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = AppConfig.memberService();//멤버서비스 생성
         //MemberService memberService = new MemberServiceImpl();
+
+        //Appconfig의 환경설정정보를 가져온다
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
 
         Member member = new Member(1L, "memberA", Grade.VIP); // 멤버 객체 생성
 

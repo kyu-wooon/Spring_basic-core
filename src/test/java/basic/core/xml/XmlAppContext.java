6ae11565ff68b720ab8bc61411630650a6e94242
DryLeonhard <ygangmin@gmail.com>
2023-09-08 15:18:15 +0900

    beanFactory / XML / BeanDefinition

---
+++ src/test/java/basic/core/xml/XmlAppContext.java
@@ -0,0 +1,22 @@
+package basic.core.xml;
+
+import basic.core.AppConfig;
+import basic.core.member.MemberService;
+import basic.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appconfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
